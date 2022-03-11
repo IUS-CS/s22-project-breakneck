bbfdec0def32e2c0f13ad325da7f3e9354622f0d
jamalail <84355230+Jamalali10@users.noreply.github.com>
2022-03-11 16:53:32 -0500

    evaluation

--- src/build evaluation model.py
+++ src/build evaluation model.py
@@ -4,12 +4,12 @@ from keras.applications.vgg16 import preprocess_input
 from keras.applications.vgg16 import decode_predictions
 from keras.applications.vgg16 import VGG16
 # load the model
-model = VGG16()
+model = VGG16();
 # load an ImageLoader from file
-ImageLoader = ImageLoader.resize((256, 256), ImageLoader.BILINEAR) 
-mlt.imshow(ImageLoader)
-ImageLoader = load_img('ImageLoader', target_size=(256, 256))
-ImageLoader = img_to_array(ImageLoader)
-ImageLoader = preprocess_input(ImageLoader)
-label = label[1000][1000]
-print('%s (%.2f%%)' % (label[1], label[2]*1000))
+ImageLoader = ImageLoader.resize((256, 256), ImageLoader.BILINEAR) ;
+mlt.imshow(ImageLoader);
+ImageLoader = load_img('ImageLoader', target_size=(256, 256));
+ImageLoader = img_to_array(ImageLoader);
+ImageLoader = preprocess_input(ImageLoader);
+label = label[1000][1000];
+print('%s (%.2f%%)' % (label[1], label[2]*1000));
